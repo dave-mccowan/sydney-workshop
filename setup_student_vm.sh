da07f508e87d8ea50a9534e6838181ea8645c2f3
Dave McCowan <dmccowan@cisco.com>
2017-10-17 21:58:27 -0400

    Combine two of the setup scripts

--- setup_student_vm.sh
+++ setup_student_vm.sh
@@ -9,6 +9,49 @@ echo "::1 localhost localhost.localdomain localhost6 localhost6.localdomain6" > 
 echo "127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4" >> /etc/hosts
 echo "$local_ip $local_host $short_host" >> /etc/hosts
 
+# setup cirros image
+mkdir -p /tmp/openstack/image/
+cp ~/cache/image/cirros* /tmp/openstack/image/
+
+############################################################
+#  Install and configure Openstack services through puppet
+############################################################
+
+cat > scenario.pp << EOF
+include ::openstack_integration
+class { '::openstack_integration::config':
+  ssl  => true,
+  ipv6 => false,
+}
+include ::openstack_integration::cacert
+include ::openstack_integration::memcached
+include ::openstack_integration::rabbitmq
+include ::openstack_integration::mysql
+include ::openstack_integration::keystone
+class { '::openstack_integration::glance':
+  backend => 'swift',
+}
+include ::openstack_integration::swift
+
+class { '::openstack_integration::nova':
+  volume_encryption => true,
+}
+
+class { '::openstack_integration::cinder':
+  volume_encryption => true,
+  cinder_backup     => 'swift',
+}
+
+include ::openstack_integration::barbican
+include ::openstack_integration::horizon
+class { '::openstack_integration::neutron':
+  driver => 'linuxbridge',
+}
+include ::openstack_integration::provision
+EOF
+
+puppet apply --modulepath /etc/puppet/modules:/usr/share/puppet/modules scenario.pp
+
 
 ####################################################
 # configure directory server for Dogtag internal DB
